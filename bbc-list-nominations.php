5c8a7a6a4792196d9b017940aa391a614480f22a
Kevin Price-Ward <kevin.price-ward@chapteragency.co.uk>
2018-03-23 16:20:42 +0000

    updates

--- bbc-list-nominations.php
+++ bbc-list-nominations.php
@@ -17,13 +17,25 @@ function print_nominations(){
     echo "<h1>PDF nominations</h1>";
     echo '<p>Use the drop down menu\'s below to select which nominations will appear on your PDF:</p>';
 ?>
-    <label for="type-select">Nomination type:</label>
-    <select name="" id="">
-        <option selected="selected" value="">- Select type -</option>
-        <option value="trade-hero">Trade Hero</option>
-        <option value="community-project">Trade Hero</option>
-        <option value="top-community-prize">Top Community Prize</option>
-    </select>
+    <div class="row">
+        <label for="type-select">Nomination type:</label>
+        <select name="" id="">
+            <option selected="selected" value="">- Select type -</option>
+            <option value="trade-hero">Trade Hero</option>
+            <option value="community-project">Trade Hero</option>
+            <option value="top-community-prize">Top Community Prize</option>
+        </select>
+    </div>
+    <div class="row">
+        <label for="region-select">Region:</label>
+        <?php
+            wp_dropdown_categories([
+                'taxonomy' => 'nomination-region',
+                'value_field' => 'slug',
+                'id' => 'region-select',
+            ]);
+        ?>
+    </div>
 <?php
     echo '</div>';
 }
