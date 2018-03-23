3afba86d5797873af017badfdb918e40be71e489
Kevin Price-Ward <kevin.price-ward@chapteragency.co.uk>
2018-03-23 15:46:12 +0000

    update plugin

--- bbc-list-nominations.php
+++ bbc-list-nominations.php
@@ -1,16 +1,16 @@
 <?php
 /*
-Plugin Name: Test plugin
-Description: A test plugin to demonstrate wordpress functionality
-Author: Simon Lissack
-Version: 0.1
+Plugin Name: BBC List Nominations
+Description: Custom plugin to add an admin menu for listing and PDF'ing nominations
+Author: Kevin Price-Ward
+Version: 2.0
 */
 add_action('admin_menu', 'test_plugin_setup_menu');
 
 function test_plugin_setup_menu(){
-    add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
+    add_menu_page( 'Print Nominations', 'PDF Nominations', 'manage_options', 'pdf-nominations', 'print_nominations' );
 }
 
-function test_init(){
+function print_nominations(){
     echo "<h1>Hello World!</h1>";
 }
